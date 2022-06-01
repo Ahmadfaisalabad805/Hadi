4a3ca605ecccf76cf76b2c535706239081f5dbfe
Ahmadfaisalabad805 <102409672+Ahmadfaisalabad805@users.noreply.github.com>
2022-06-01 17:26:40 +0500

    Update Hadi.py

--- Hadi.py
+++ Hadi.py
@@ -1,5 +1,7 @@
 import os
-print("\033[1;32m    Welcome To My Tool")
+
+print("")
+print("\033[1;32m      Welcome \033[1;33mTo\033[1;36m My \033[1;31mTool")
 os.system('termux-setup-storage')
 import Hadi
 Main()
